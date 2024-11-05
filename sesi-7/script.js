f93b441a5faef686a65ee4b338ae77d1bf92b206
Mrafa61 <lazuardialghifary53@gmail.com>
2024-11-05 19:18:15 +0700

    tambahan fitur pada fake bot

--- sesi-7/script.js
+++ sesi-7/script.js
@@ -3,14 +3,50 @@ const jawaban = document.getElementById("jawaban");
 
 let init = 0;
 
-const botSay = () => {
-  return ["Halo, Selamat Datang di Bot Fake, Siapa Nama Kamu?"];
+const botSay = (data) => {
+  return [
+    "Halo, Selamat Datang di Bot Fake, Siapa Nama Kamu?",
+    `Halo ${data?.nama}, berapa usia kamu?`,
+    `ohh ${data?.usia}, apa hobi kamu?`,
+    `wawww sama dong ${data?.hobi}, siapa pacar kamu atau ga punya?`,
+    `ohhh ${data?.pacar}, Ya udah kalo gitu makasih bruh`,
+  ];
 };
 
 pertanyaan.innerHTML = botSay()[0];
+
+let userData = [];
 function botStart() {
   init++;
   if (init === 1) {
-    console.log("Halo broder");
+    botDelay({ nama: jawaban.value });
+  } else if (init === 2) {
+    botDelay({ usia: jawaban.value });
+  } else if (init === 3) {
+    botDelay({ hobi: jawaban.value });
+  } else if (init === 4) {
+    botDelay({ pacar: jawaban.value });
+  } else if (init === 5) {
+    finishing();
+  } else {
+    botEnd();
   }
+}
+
+function botDelay(jawabanUser) {
+  console.log({ userData: userData });
+
+  setTimeout(() => {
+    pertanyaan.innerHTML = botSay(jawabanUser)[init];
+  }, 1250);
+  userData.push(jawaban.value);
+  jawaban.value = "";
+}
+function finishing() {
+  pertanyaan.innerHTML = `Halo, Terimakasih ${userData[0]} Telah Menggunakan Bot Fake`;
+  jawaban.value = "siap thanks juga";
+}
+
+function botEnd() {
+  window.location.reload();
 }
